15218838ff5c41d6288994db2b7f1961542d3cf8
Mike Melikhan H <62219260+melikhanhosdogdu@users.noreply.github.com>
2025-07-20 13:15:14 -0500

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+let groceryItems = ["Bananas", "Cereal", "Yogurt", "Juice", "Chicken"];
+let prices = [1.25, 4.50, 3.10];
+
+console.log("Grocery Items:", groceryItems);
+console.log("Prices:", prices);
+
+console.log(groceryItems[1]);
+console.log(groceryItems[3]);
+console.log(groceryItems[0]);
+
+console.log(prices[1]);
+console.log(prices[2]);
